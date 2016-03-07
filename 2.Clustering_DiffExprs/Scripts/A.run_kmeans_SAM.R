d1b678c60b9fa5bad5a5b60a818dfeb90af40901
changt34x <changt34x@gmail.com>
2016-03-07 11:58:11 -0500

    Update A.run_kmeans_SAM.R
    Update with working copy of the script

--- 2.Clustering_DiffExprs/Scripts/A.run_kmeans_SAM.R
+++ 2.Clustering_DiffExprs/Scripts/A.run_kmeans_SAM.R
@@ -1,5 +1,5 @@
 ############################################
-# Cross-population analysis of high-grade serous ovarian cancer reveals only two robust subtypes
+# High-grade serous ovarian cancer subtypes are similar across populations
 # 
 # Way, G.P., Rudd, J., Wang, C., Hamidi, H., Fridley, L.B,  
 # Konecny, G., Goode, E., Greene, C.S., Doherty, J.A.
