eaca64b1cff1fa0e3ce7ec9ab262dbf150aa9992
amyecampbell <amyecampbell@gmail.com>
2017-05-19 13:29:00 -0400

    formatting

--- 1.DataInclusion/Scripts/Functions/LoadOVCA_Data.R
+++ 1.DataInclusion/Scripts/Functions/LoadOVCA_Data.R
@@ -1,5 +1,6 @@
  ############################################
-# Cross-population analysis of high-grade serous ovarian cancer reveals only two robust subtypes
+# Cross-population analysis of high-grade serous ovarian cancer reveals
+# only two robust subtypes
 #
 # Way, G.P., Rudd, J., Wang, C., Hamidi, H., Fridley, L.B,  
 # Konecny, G., Goode, E., Greene, C.S., Doherty, J.A.
@@ -16,23 +17,28 @@ library(plyr)
 ############################################
 # Load Data Function
 ############################################
-LoadOVCA_Data <- function(datasets, goodsample_subset_dir = "1.DataInclusion/Data/GoodSamples/", 
+LoadOVCA_Data <- function(datasets,
+                          goodsample_subset_dir = "1.DataInclusion/Data/GoodSamples/",
                           commongenes_dir = "1.DataInclusion/Data/Genes/CommonGenes_genelist.csv",
                           madgenes_dir = "1.DataInclusion/Data/Genes/GlobalMAD_genelist.csv",
                           genelist_subset = "commongenes",
                           mayo_exprs_file = "1.DataInclusion/Data/Mayo/MayoEset.Rda",
+                          aaces_path = "expression.tsv",
                           shuffle = F) {
 # ~~~~~~~~~~~~~~
   # Loads ovarian cancer data from curatedOvarianData
   #
   # Args: 
   # datasets: a list of datasets to download
-  # goodsample_subset_dir: the directory holds .txt files of samples that passed inclusion
-  #                        if no sample subsetting is desired, set this argument to "None"
+  # goodsample_subset_dir: the directory holds .txt files of samples that
+  # passed inclusion
+  # if no sample subsetting is desired, set this argument to "None"
   # commongenes_dir: the file location of the common genes .txt file
-  # madgenes_dir: the file location of the mad (median absolute deviation) genes .txt file
+  # madgenes_dir: the file location of the mad (median absolute deviation)
+  # genes .txt file 
   # genelist_subset: the genelist to subset each dataset
-  # shuffle: determines if the genelists should be shuffled in the final expression matrix
+  # shuffle: determines if the genelists should be shuffled in the final
+  # expression matrix
   #
   # Returns:
   # A list object holding the gene expression data for the
@@ -40,18 +46,18 @@ LoadOVCA_Data <- function(datasets, goodsample_subset_dir = "1.DataInclusion/Dat
   # ~~~~~~~~~~~~~~
   
   # Get data from the curatedOvarianData package  
-  detailedData <- data(package="curatedOvarianData")[3]
+  detailedData <- data(package = "curatedOvarianData")[3]
   # Get the eset IDs
-  detailedData.names <- detailedData$results[ ,3]
-  
+  detailedData.names <- detailedData$results[, 3]
+  (rownames(exprs(get("TCGA_eset"))))[400:900]
   # Initialize a list object
   ExpData <- list()
   for (eset_exprs in datasets) {
     if (goodsample_subset_dir != "None") {
       # Get the list of samples to remove
-      fileName <- paste(eset_exprs, "_samplesRemoved.csv",sep="")
-      goodSamples <- read.csv(file = paste(goodsample_subset_dir, fileName, sep = ""))
-      
+      fileName <- paste(eset_exprs, "_samplesRemoved.csv", sep = "")
+      goodSamples <- read.csv(file = paste(goodsample_subset_dir,
+                                           fileName, sep = ""))
       # The samples to use in the subset
       goodSamples <- c(paste(goodSamples$x))
     }
@@ -72,9 +78,9 @@ LoadOVCA_Data <- function(datasets, goodsample_subset_dir = "1.DataInclusion/Dat
     } else if (grepl("aaces", eset_exprs)) {
       cat("Loading", eset_exprs, "...\n")
       # AACES eset
-      dta <- read.table("expression.tsv", sep = "\t", row.names = 1, header = TRUE)
+      dta <- read.table(aaces_path, sep = "\t", row.names = 1,
+                        header = TRUE)
     } else {
-      print(eset_exprs)
       stop("Dataset does not exist in curatedOvarianData")
     }
 
@@ -90,22 +96,24 @@ LoadOVCA_Data <- function(datasets, goodsample_subset_dir = "1.DataInclusion/Dat
     
     # Assign the list elements according to the subset of choice
     if (goodsample_subset_dir != "None" & genelist_subset != "None") {
-      ExpData[[eset_exprs]] <- dta[subset[ ,1], goodSamples]
+      #dta <- dplyr::filter(rownames(dta) %in%)
+      ExpData[[eset_exprs]] <- dta[subset[, 1], goodSamples]
     } else if (goodsample_subset_dir == "None" & genelist_subset != "None") {
-      ExpData[[eset_exprs]] <- dta[subset[ ,1], ]
+      ExpData[[eset_exprs]] <- dta[subset[, 1], ]
     } else {
-      ExpData[[eset_exprs]] <- dta[ ,goodSamples]
+      ExpData[[eset_exprs]] <- dta[, goodSamples]
     }
     
     if (shuffle) {
       for (gene in 1:nrow(ExpData[[eset_exprs]])) {
-        random_vec <- sample(ExpData[[eset_exprs]][gene, ], ncol(ExpData[[eset_exprs]]))
+        random_vec <- sample(ExpData[[eset_exprs]][gene, ],
+                             ncol(ExpData[[eset_exprs]]))
         ExpData[[eset_exprs]][gene, ] <- random_vec
       }
     }
     
   }
-  #Return a list of subsetted gene expression data.frames for all input datasets
+  # Return a list of subsetted gene expression data.frames for all
+  # input datasets
   return(ExpData)
 }
-
