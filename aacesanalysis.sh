59cc4444c623e56d6b75bdcbd3c2d30b864b23c5
amyecampbell <amyecampbell@gmail.com>
2017-05-22 12:02:23 -0400

    aaces shell script

--- aacesanalysis.sh
+++ aacesanalysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec &>hgsc_analysis.out 
+#exec &>hgsc_analysis.out 
 
 ############################################
 # Based on shell script from:
@@ -97,95 +97,3 @@ $DATASETS "GSE26712_eset" < 2.Clustering_DiffExprs/Scripts/A.run_kmeans_SAM.R
 R --no-save --args $KMIN $KMAX $SEED Figures/CorrelationMatrix/ $DATASETS \
 "GSE26712_eset" < 2.Clustering_DiffExprs/Scripts/B.CorrelationMatrix.R
 
-# Output k-means barcharts (Figure S8)
-R --no-save --args $KMIN $KMAX $DATASETS < 2.Clustering_DiffExprs/Scripts/\
-C.KMeansBarCharts.R
-
-# Shuffle genes to compare across population correlations in real data
-R --no-save --args $KMIN $KMAX $NSTARTS $SEED FALSE $SHUFFLE $SAM_SUBSET \
-$DATASETS "GSE26712_eset" < 2.Clustering_DiffExprs/Scripts/A.run_kmeans_SAM.R
-
-# ~~~~~~~~~~~~~
-# NMF
-# ~~~~~~~~~~~~~
-# Output consensus matrices, NMF cluster membership files (Sup. Figure S2) and 
-# cophenetic coefficients (Sup. Figures S3-S7)
-R --no-save --args $KMIN $KMAX $NSTARTS $SEED $DATASETS "GSE26712_eset" \
-< 2.Clustering_DiffExprs/Scripts/D.NMF.R
-
-# Run SAM on NMF clusters (TRUE argument forces NMF analysis)
-R --no-save --args $KMIN $KMAX $NSTARTS $SEED TRUE $NO_SHUFFLE $SAM_SUBSET \
-$DATASETS "GSE26712_eset" < 2.Clustering_DiffExprs/Scripts/A.run_kmeans_SAM.R
-
-# ~~~~~~~~~~~~~
-# k means vs. NMF
-# ~~~~~~~~~~~~~
-# Compare k-means defined clusters with NMF defined clusters (Figure 2)
-R --no-save --args $DATASETS < 2.Clustering_DiffExprs/Scripts/E.kmeans_v_nmf.R
-
-# Compile table with all cluster membership information (Sup. Table S2)
-R --no-save --args $DATASETS < 2.Clustering_DiffExprs/Scripts/\
-F.clusterMembership.R
-
-# ~~~~~~~~~~~~~
-# Dataset Concordance
-# ~~~~~~~~~~~~~
-# Investigate the similarities in cluster membership in original TCGA 2011 
-# paper, the Konecny 2014 paper, and the Tothill 2008 paper (Table 4)
-R --no-save < 2.Clustering_DiffExprs/Scripts/G.Dataset_concordance.R
-
-# ~~~~~~~~~~~~~
-# Tothill LMP
-# ~~~~~~~~~~~~~
-# Observe consensus matrices and cophenetic coefficients for Tothill dataset if 
-# LMP samples are not removed. This is similar to the results presented by TCGA 
-# supplementary figure S6.2 (Figure 3)
-R --no-save < 2.Clustering_DiffExprs/Scripts/H.TCGA_LMP_TothillPrediction.R
-
-#################
-# PART THREE: 
-# Goodness of Fit
-#################
-# ~~~~~~~~~~~~~~~~~~~~~
-# The section will output several figures, all different goodness of fit 
-# metrics, for each dataset. The metrics include AIC, BIC, Gap Statistic, and 
-# Silhouette Widths (Note: Cophenetic obtained in step 2)
-# ~~~~~~~~~~~~~~~~~~~~~
-
-# Perform AIC, BIC, and silhouette width analyses
-R --no-save --args 2 8 20 $NSTARTS $SEED $DATASETS < \
-3.Fit/Scripts/A.GoodnessFit.R
-
-# Determine gap statistic
-# NOTE: This step was performed on the Discovery Cluster at Dartmouth College
-# R --no-save --args 8 250 20 50 $SEED $DATSETS < 3.Fit/Scripts/\
-# B.GAP_GoodnessFit.R
-
-#################
-# PART FOUR: 
-# Survival
-#################
-# ~~~~~~~~~~~~~~~~~~~~~
-# This section will perform all survival analyses and output summary tables
-# ~~~~~~~~~~~~~~~~~~~~~
-
-# Output Kaplan-Meier survival curves, and perform a cox proportional hazards 
-# regression model (Sup. Fig. S11)
-R --no-save --args $DATASETS < 4.Survival/Scripts/A.Survival.R
-
-# Summarize the results of the survival analysis (Sup. Table S3)
-R --no-save --args $DATASETS < 4.Survival/Scripts/B.Summarize_Survival.R
-
-#################
-# PART FIVE: 
-# Gene and pathway Analyses
-#################
-# ~~~~~~~~~~~~~~~~~~~~~
-# Describe the cluster driving genes in common across populations
-# ~~~~~~~~~~~~~~~~~~~~~
-
-# Output tables of cluster specific genes based on significant SAM FDR values
-R --no-save --args $DATASETS < 5.Pathway/Scripts/A.GeneEnrichment.R
-
-# The output of this script is input into a PANTHER pathways analysis
-# (http://pantherdb.org/)
