bc55c3f83ed7e7d73a9a9ad942b87d14b2963bf3
changt34x <changt34x@gmail.com>
2017-06-06 16:20:30 -0500

    update ESTIMATE script

--- 6.Immune_Infiltrate/Scripts/A.ESTIMATE.R
+++ 6.Immune_Infiltrate/Scripts/A.ESTIMATE.R
@@ -1,11 +1,21 @@
-############################################
-# Cross-population analysis of high-grade serous ovarian cancer does not support four subtypes
-# 
-# Way, G.P., Rudd, J., Wang, C., Hamidi, H., Fridley, L.B,  
-# Konecny, G., Goode, E., Greene, C.S., Doherty, J.A.
-# ~~~~~~~~~~~~~~~~~~~~~
-# This script will perform ESTIMATE analysis on the datasets and output infiltration
-# results for all input datasets
+# Timothy Chang 2017
+# ESTIMATE Tumor Purity Analysis
+# 6.Immune_Infiltrate/Scripts/A.ESTIMATE.R
+#
+# Analyzes tumor purity with the ESTIMATE algorithm and plots 
+# Stromal/Immune infiltrate and tumor purity by subtype
+#
+# Usage: Run by selecting ovarian cancer datasets to be analyzed
+#
+#     Rscript 6.Immune_Infiltrate/Scripts/A.ESTIMATE.R
+#
+#     with the required argument:
+#         --args        Datasets to be analyzed
+#
+# Output:
+# Tables with Stromal, Immune and ESTIMATE score along with tumor purity
+# Box plots by subtype for ESTIMATE score and tumor purity
+# Scatter plots by subtype for comparing Stromal and Immune score
 
 suppressMessages(library(checkpoint))
 suppressMessages(checkpoint('2016-03-01', checkpointLocation = "."))
@@ -23,9 +33,12 @@ library(reshape2)
 library(ggplot2)
 library(gridExtra)
 library(grid)
+library(dplyr)
 
-# The script loads the ovarian cancer datasets
-source("1.DataInclusion/Scripts/Functions/LoadOVCA_Data.R")
+# Loads curatedOvarianData
+loadovcaPath <- file.path("1.DataInclusion", "Scripts", "Functions",
+                          "LoadOVCA_Data.R")
+source(loadovcaPath)
 
 ############################################
 # Data in curatedOvarianData
@@ -48,8 +61,17 @@ if ("mayo.eset" %in% args) {
 ############################################
 # Load Data
 ############################################
-# Use the LoadOVCA_Data function to read in the datasets subset by commongenes
-ExpData <- LoadOVCA_Data(datasets = argsCurated, genelist_subset = "commongenes")
+# Use the LoadOVCA_Data function to read in the datasets
+ExpData <- LoadOVCA_Data(datasets = argsCurated, 
+                         genelist_subset = "commongenes",
+                         zscore = TRUE)
+
+# Write data to disk for ESTIMATE functions
+for (dataset in names(ExpData)) {
+  fName <- paste0(dataset, "_processed.gct")
+  fPath <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", fName)
+  write.table(ExpData[[dataset]], fPath, sep = '\t', col.names = NA, quote = FALSE)
+}
 
 ############################################
 # Get Cluster Membership Info
@@ -58,96 +80,68 @@ ExpData <- LoadOVCA_Data(datasets = argsCurated, genelist_subset = "commongenes"
 # according to the order of the datasets passed as arguments
 ClusterMembershipList <- list()
 ClusterMembershipList_NMF <- list()
-datasetMembers <- list.files(path = "2.Clustering_DiffExprs/Tables/ClusterMembership/kmeans/")
-datasetMembers_NMF <- list.files(path = "2.Clustering_DiffExprs/Tables/ClusterMembership/nmf/") 
-for (dataset in 1:length(args)) {
-  holder <- args[dataset]
+
+kmeansPath <- file.path("2.Clustering_DiffExprs", "Tables", "ClusterMembership", "kmeans")
+datasetMembers <- list.files(path = kmeansPath)
+
+nmfPath <- file.path("2.Clustering_DiffExprs", "Tables", "ClusterMembership", "nmf")
+datasetMembers_NMF <- list.files(path = nmfPath) 
+
+for (dataset in args) {
+  holder <- dataset
+  
+  # kmeans cluster membership
   ClusMember <- datasetMembers[grep(holder, datasetMembers)]
-  
-  ClusData <- read.csv(paste("2.Clustering_DiffExprs/Tables/ClusterMembership/kmeans/", 
-                             ClusMember, sep = ""), row.names = 1)
-  
+  kmeansFile <- file.path(kmeansPath, ClusMember)
+  ClusData <- read.csv(kmeansFile, stringsAsFactors = FALSE)
+  names(ClusData)[1] <- "Sample"
   ClusterMembershipList[[dataset]] <- ClusData
-  names(ClusterMembershipList)[dataset] <- args[dataset]
   
   # NMF cluster membership
   ClusMember <- datasetMembers_NMF[grep(holder, datasetMembers_NMF)]
   ClusMember <- ClusMember[grepl("mapped", ClusMember)]
-  ClusData <- read.csv(paste("2.Clustering_DiffExprs/Tables/ClusterMembership/nmf/", 
-                             ClusMember, sep = ""), row.names = 1)
-  
+  nmfFile <- file.path(nmfPath, ClusMember)
+  ClusData <- read.csv(nmfFile, stringsAsFactors = FALSE)
+  names(ClusData)[1] <- "Sample"
   ClusterMembershipList_NMF[[dataset]] <- ClusData
-  names(ClusterMembershipList_NMF)[dataset] <- args[dataset]
-}
-
-############################################
-# Compute z-Score
-############################################
-# Function to compute z scores
-z_score_func <- function(x) {
-  z <- (x - mean(x)) / sd(x)
-  return(z)
-}
-
-# Compute z-score by gene
-for (dataset in 1:length(ExpData)) {
-  for (gene in 1:dim(ExpData[[dataset]])[2]) {
-    ExpData[[dataset]][, gene] <- z_score_func(ExpData[[dataset]][, gene])
-  }
-
-  # Write dataset to disk
-  fName <- paste0(names(ExpData)[dataset], '_processed.gct')
-  write.table(ExpData[[dataset]], 
-              file = file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", fName),
-              sep = "\t",
-              col.names = NA,
-              quote = FALSE)
 }
 
 ############################################
 # Compute ESTIMATE Scores
 ############################################
-isAffymetrix <- c()
+datasetPlatform <- c()
 
-for (dataset in 1:length(ExpData)) {
-  fName <- paste0(names(ExpData)[dataset], '_processed.gct')
-  fName1 <- paste0(names(ExpData)[dataset], '_filtered.gct')
-  fName2 <- paste0(names(ExpData)[dataset], '_estimate.gct')
+for (dataset in args) {
+  processedFile <- paste0(dataset, "_processed.gct")
+  filteredFile <- paste0(dataset, "_filtered.gct")
+  estimateFile <- paste0(dataset, "_estimate.gct")
 
   # Filter for common genes in the ESTIMATE function
-  filterCommonGenes(input.f = file.path("6.Immune_Infiltrate", "Tables",
-                                        "ESTIMATE", fName),
-                    output.f = file.path("6.Immune_Infiltrate", "Tables",
-                                         "ESTIMATE", fName1),
+  inputFile <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", processedFile)
+  outputFile <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", filteredFile)
+  filterCommonGenes(input.f = inputFile,
+                    output.f = outputFile,
                     id = "GeneSymbol")
 
   # Check if Mayo (Agilent)
-  if (grepl("mayo", names(ExpData)[dataset], ignore.case = TRUE)) {
-    estimateScore(input.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                       "ESTIMATE", fName1),
-                  output.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                        "ESTIMATE", fName2),
-                  platform = "agilent")
-    isAffymetrix[dataset] <- FALSE
-  # Run Agilent-based datasets
+  if (dataset == "mayo.eset") {
+    datasetPlatform[dataset] <- "agilent"
+  # Check if dataset is based on the Agilent platform
   } else if (grepl("Agilent", 
-             experimentData(eval(as.name(names(ExpData)[dataset])))@other$platform_manufacturer,
+             experimentData(eval(as.name(dataset)))@other$platform_manufacturer,
              ignore.case = TRUE)) {
-    estimateScore(input.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                       "ESTIMATE", fName1),
-                  output.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                        "ESTIMATE", fName2),
-                  platform = "agilent")
-    isAffymetrix[dataset] <- FALSE
-  # Run Affymetrix-based datasets
+    datasetPlatform[dataset] <- "agilent"
+  # Run remaining sets as Affymetrix
   } else {
-    estimateScore(input.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                       "ESTIMATE", fName1 ),
-                  output.ds = file.path("6.Immune_Infiltrate", "Tables",
-                                        "ESTIMATE", fName2),
-                  platform = "affymetrix")
-    isAffymetrix[dataset] <- TRUE
+    datasetPlatform[dataset] <- "affymetrix"
   }
+
+  # Run ESTIMATE algorithm
+  inputFile <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", filteredFile)
+  outputFile <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE", estimateFile)
+  estimateScore(input.ds = inputFile,
+                output.ds = outputFile,
+                platform = datasetPlatform[dataset])
 }
 
 ############################################
@@ -156,25 +150,29 @@ for (dataset in 1:length(ExpData)) {
 # This loop will pull all ESTIMATE files and load them into a list 
 # according to the order of the datasets passed as arguments
 ESTIMATEResultsList <- list()
-datasetMembers <- list.files(path = "6.Immune_Infiltrate/Tables/ESTIMATE/")
-for (dataset in 1:length(args)) {
-  holder <- args[dataset]
+
+estimatePath <- file.path("6.Immune_Infiltrate", "Tables", "ESTIMATE")
+datasetMembers <- list.files(path = estimatePath)
+
+for (dataset in args) {
+  holder <- dataset
   EstMembers <- datasetMembers[grep(holder, datasetMembers)]
   EstMember <- EstMembers[grep("estimate", EstMembers)]
 
   # Read and clean up input table
-  EstData <- read.table(file = paste0("6.Immune_Infiltrate/Tables/ESTIMATE/", 
-                                      EstMember),
+  estimateFile <- file.path(estimatePath, EstMember)
+  EstData <- read.table(file = estimateFile,
                         sep = "\t",
                         skip = 2,
-                        header = T)
+                        header = TRUE,
+                        stringsAsFactors = FALSE)
   rownames(EstData) <- EstData[, 2]
   EstData$NAME <- NULL
   EstData$Description <- NULL
   EstData <- t(EstData)
+  EstData <- tibble::rownames_to_column(as.data.frame(EstData), "Sample")
 
   ESTIMATEResultsList[[dataset]] <- EstData
-  names(ESTIMATEResultsList)[dataset] <- args[dataset]
 }
 
 # Duplicate table for NMF
@@ -183,22 +181,27 @@ ESTIMATEResultsList_NMF <- ESTIMATEResultsList
 ############################################
 # Merge ESTIMATE scores and clustering data
 ############################################
-for (dataset in 1:length(args)) {
-  ESTIMATEResultsList[[dataset]] <- merge(x = ESTIMATEResultsList[[dataset]], 
-                                          y = ClusterMembershipList[[dataset]], 
-                                          by = "row.names")
-  ESTIMATEResultsList_NMF[[dataset]] <- merge(x = ESTIMATEResultsList[[dataset]],
-                                              y = ClusterMembershipList_NMF[[dataset]],
-                                              by = "row.names")
+for (dataset in args) {
+  ESTIMATEResultsList[[dataset]] <- inner_join(ESTIMATEResultsList[[dataset]], 
+                                               ClusterMembershipList[[dataset]],
+                                               by = "Sample")
+  ESTIMATEResultsList_NMF[[dataset]] <- inner_join(ESTIMATEResultsList[[dataset]],
+                                                   ClusterMembershipList_NMF[[dataset]],
+                                                   by = "Sample")
 }
 
 ############################################
 # Analaysis of ESTIMATE and Tumor Purity
 ############################################
 k_list <- c("K2", "K3", "K4")
+boxplot_theme <- theme(panel.background = element_blank(),
+                       panel.border = element_rect(color = "black", fill = NA),
+                       axis.text.y = element_text(size = 14, angle = 90,
+                                                  hjust = 0.4, colour = "black"),
+                       axis.text.x = element_text(size = 14, colour = "black"))
 
 # Analysis with k-means clustering
-for (dataset in 1:length(args)) {
+for (dataset in args) {
   ESTIMATEPlots <- list()
   PurityPlots <- list()
 
@@ -209,8 +212,8 @@ for (dataset in 1:length(args)) {
     num_clus <- gsub("K", "", k)
 
     # Extract dataframe with scores and clustering
-    est_df <- ESTIMATEResultsList[[dataset]][c("Row.names", "ESTIMATEScore",
-                                               paste0("Cluster", k))]
+    est_rows <- c("Sample", "ESTIMATEScore", paste0("Cluster", k))
+    est_df <- ESTIMATEResultsList[[dataset]][est_rows]
     colnames(est_df)[3] <- "Cluster"
 
     # Plot ESTIMATE Score by subtype
@@ -219,18 +222,14 @@ for (dataset in 1:length(args)) {
       geom_boxplot() +
       scale_x_discrete(limits = paste(1:num_clus)) + 
       labs(title = "", x = "", y = "") +
-      theme(panel.background = element_blank(),
-            panel.border = element_rect(color = "black", fill = NA),
-            axis.text.y = element_text(size = 14, angle = 90,
-                                       hjust = 0.4, colour = "black"),
-            axis.text.x = element_text(size = 14, colour = "black"))
+      boxplot_theme
     ESTIMATEPlots[[data_iter]] <- e
 
     # Analyze tumor purity for Affymetrix datasets
-    if (isAffymetrix[dataset]) {
+    if (datasetPlatform[dataset] == "affymetrix") {
       # Extract dataframe with purity and clustering
-      pur_df <- ESTIMATEResultsList[[dataset]][c("Row.names", "TumorPurity",
-                                                 paste0("Cluster", k))]
+      pur_rows <- c("Sample", "TumorPurity", paste0("Cluster", k))
+      pur_df <- ESTIMATEResultsList[[dataset]][pur_rows]
       colnames(pur_df)[3] <- "Cluster"
 
       # Plot Tumor Purity by subtype
@@ -239,11 +238,7 @@ for (dataset in 1:length(args)) {
         geom_boxplot() +
         scale_x_discrete(limits = paste(1:num_clus)) + 
         labs(title = "", x = "", y = "") +
-        theme(panel.background = element_blank(),
-              panel.border = element_rect(color = "black", fill = NA),
-              axis.text.y = element_text(size = 14, angle = 90,
-                                        hjust = 0.4, colour = "black"),
-              axis.text.x = element_text(size = 14, colour = "black"))
+        boxplot_theme
       PurityPlots[[data_iter]] <- p
     }
 
@@ -258,14 +253,15 @@ for (dataset in 1:length(args)) {
   est_plot_eval <- paste0(est_plot_eval, "ncol = length(ESTIMATEPlots)", ", nrow = 1)")
 
   # Write plot to PNG
-  fName <- paste0(args[dataset], "_ESTIMATEScore_kmeans.png")
-  png(filename = file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName),
+  fName <- paste0(dataset, "_ESTIMATEScore_kmeans.png")
+  fPath <- file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName)
+  png(filename = fPath,
       width = 400 * length(ESTIMATEPlots),
       height = 400)
   eval(parse(text = est_plot_eval))
   dev.off()
 
-  if (isAffymetrix[dataset]) {
+  if (datasetPlatform[dataset] == "affymetrix") {
     # Build a string to evaluate Tumor Purity plots
     pur_plot_eval <- "full_grobs <- grid.arrange("
     for (i in 1:length(PurityPlots)) {
@@ -274,8 +270,9 @@ for (dataset in 1:length(args)) {
     pur_plot_eval <- paste0(pur_plot_eval, "ncol = length(PurityPlots)", ", nrow = 1)")
 
     # Write plot to PNG
-    fName <- paste0(args[dataset], "_TumorPurity_kmeans.png")
-    png(filename = file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName),
+    fName <- paste0(dataset, "_TumorPurity_kmeans.png")
+    fPath <- file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName)
+    png(filename = fPath,
         width = 400 * length(PurityPlots),
         height = 400)
     eval(parse(text = pur_plot_eval))
@@ -286,7 +283,7 @@ for (dataset in 1:length(args)) {
 ############################################
 # Analaysis of Stromal and Immune Scores
 ############################################
-for (dataset in 1:length(args)) {
+for (dataset in args) {
   StromalImmunePlots <- list()
 
   data_iter <- 1
@@ -295,15 +292,15 @@ for (dataset in 1:length(args)) {
     num_clus <- gsub("K", "", k)
 
     # Extract dataframe with scores and clustering
-    svi_df <- ESTIMATEResultsList[[dataset]][c("Row.names", "StromalScore",
-                                               "ImmuneScore", paste0("Cluster", k))]
+    svi_rows <- c("Sample", "StromalScore", "ImmuneScore", paste0("Cluster", k))
+    svi_df <- ESTIMATEResultsList[[dataset]][svi_rows]
     colnames(svi_df)[4] <- "Cluster"
 
     # Plot scatter plot of Stromal vs. Immune Score by subtype
     s <- ggplot(data = svi_df,
                 mapping = aes(x = StromalScore, y = ImmuneScore, color = factor(Cluster))) +
       geom_point(cex = 2.5) +
-      scale_color_brewer(palette = "Set1") +
+      scale_colour_manual(values = c("skyblue1", "tomato", "springgreen", "violet")) +
       labs(title = "",
            color = "Subtype",
            x = "Stromal Score",
@@ -331,8 +328,9 @@ for (dataset in 1:length(args)) {
   svi_plot_eval <- paste0(svi_plot_eval, "ncol = length(StromalImmunePlots)", ", nrow = 1)")
 
   # Write plot to PNG
-  fName <- paste0(args[dataset], "_StromalvImmune_kmeans.png")
-  png(filename = file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName),
+  fName <- paste0(dataset, "_StromalvImmune_kmeans.png")
+  fPath <- file.path("6.Immune_Infiltrate", "Figures", "ESTIMATE", fName)
+  png(filename = fPath,
       width = 800 * length(StromalImmunePlots),
       height = 650)
   eval(parse(text = svi_plot_eval))
